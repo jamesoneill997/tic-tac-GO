9b7311c4bf4b8fe8f6c5b0cc2ec0535589394f35
jamesoneill997 <jamesoneill997@gmail.com>
2020-11-12 01:23:11 +0000

    added drawboard and greeting function

--- pkg/app.go
+++ pkg/app.go
@@ -23,14 +23,17 @@ func intro(player1 string, player2 string) {
 	fmt.Println("Welcome to tic-tac-GO. To play, simply enter the square number that you would like to place your mark on. Get 3 in a vertical or horizontal row to win!")
 }
 
-//initBoard creates a blank board
-func initBoard() {
-	for i := 0; i <= 12; i++ {
-		if i == 4 || i == 8 {
+func drawBoard(tiles [9]string) {
+	for i, j := 0, 0; i <= 13; i++ {
+		if i == 4 || i == 9 {
 			fmt.Println(strings.Repeat("-", 25))
+		} else if i%5 == 1 {
+			fmt.Println(strings.Repeat(" ", 2), tiles[j], " ", "|", strings.Repeat(" ", 2), tiles[j+1], " ", "|", strings.Repeat(" ", 2), tiles[j+2], " ")
+			j += 3
 		} else {
 			fmt.Println(strings.Repeat(" ", 6), "|", strings.Repeat(" ", 6), "|")
 		}
+
 	}
 }
 
@@ -43,5 +46,7 @@ func main() {
 
 	intro(player1, player2)
 
-	initBoard()
+	tiles := [9]string{"0", "1", "2", "3", "4", "5", "6", "7", "8"}
+
+	drawBoard(tiles)
 }
